07bb5e4aaf6e9f21bd328a38cd045a5aa4c7fca1
hairyElephant <josecalderon353@gmail.com>
2023-03-03 17:22:41 -0500

    removes extra code and adds comments

--- leetCode.js
+++ leetCode.js
@@ -1,11 +1,17 @@
 function twoSum(nums, target){
+    // answer variable will store answers
     let answer = []
+    // first 'for loop' is grabbing a number
     for(i in nums){
+        // this 'for loop' is iterating through array and comparing each number to the number grabbed by the parent 'for loop'
         for(j in nums){
+            // conditional statement ensuring the same instance of a number isn't used twice
             if(i == j){
                 continue
             }
+            // conditional statement checking if the two selected numbers add up to the target
             else if(nums[i] + nums[j] == target){
+                //.push() is adding indices to the answer array from above
                 answer.push(i,j)
                 return answer
             }
@@ -13,6 +19,3 @@ function twoSum(nums, target){
     }
 }
 
-let random = [0,2,9,8,3]
-
-console.log(twoSum(random, 5))
